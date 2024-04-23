f8cdd01ebe1f802a958b3a184262edbca4dacaf8
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-04-23 16:55:08 +0100

    Update after multiple changes

--- client/src/components/CarouselSliders.js
+++ client/src/components/CarouselSliders.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+import YouTube from "react-youtube";
 import styles from "./styles/CarouselSliders.module.css";
 
 const CarouselSliders = (props) => {
   const [activeIdx, setActiveIdx] = React.useState(0);
+  const players = useRef({});
+
+  const opts = {
+    // width: "420",
+    // height: "210",
+  };
+
+  const onReady = (event, videoId) => {
+    players.current[videoId] = event.target;
+  };
+
+  const onPlay = (event) => {
+    const videoId = event.target.getVideoData().video_id;
+    // Pause other videos
+    Object.values(players.current).forEach((player) => {
+      const otherVideoId = player.getVideoData().video_id;
+      if (
+        otherVideoId !== videoId &&
+        player.getPlayerState() === 1 /* Playing */
+      ) {
+        player.pauseVideo();
+      }
+    });
+  };
 
   return (
     <>
@@ -15,22 +40,22 @@ const CarouselSliders = (props) => {
                 parseInt(h, 10) * 360 + parseInt(m, 10) * 60 + parseInt(s, 10);
 
               return (
-                <>
-                  <div
-                    key={index}
-                    id={`slide-${index}`}
-                    className={styles.SlideItem}
-                  >
-                    <iframe
-                      className="iframe-youtube"
-                      src={`https://www.youtube.com/embed/${match.youtubeid}?start=${timestamp}`}
-                      title={match.songname}
-                      frameBorder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                      allowFullScreen
-                    ></iframe>
-                  </div>
-                </>
+                <div
+                  key={index}
+                  id={`slide-${index}`}
+                  className={styles.SlideItem}
+                >
+                  <YouTube
+                    videoId={match.youtubeid}
+                    opts={{
+                      ...opts,
+                      playerVars: { start: timestamp, rel: 0 },
+                    }}
+                    iframeClassName={styles.Iframe}
+                    onReady={(event) => onReady(event, match.youtubeid)}
+                    onPlay={onPlay}
+                  />
+                </div>
               );
             })}
           </div>
