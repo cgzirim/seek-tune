b036ec0c3d79414940cd23f1f254915e61662712
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-05-17 12:42:08 +0100

    Update BeforeInstall.sh

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -3,24 +3,35 @@
 # clear codedeploy-agent files for a fresh install
 # sudo rm -rf /home/ubuntu/install
 
-touch /home/ubuntu/hello.txt
-
-# install CodeDeploy agent
+touch /home/ubuntu/status.txt
 sudo apt-get -y update
-sudo apt-get -y install ruby
-sudo apt-get -y install wget
-cd /home/ubuntu
-wget https://aws-codedeploy-eu-north-1.s3.amazonaws.com/latest/install
-sudo chmod +x ./install 
-sudo ./install auto
+
+if [ ! -d "/home/ubuntu/install" ]; then
+    # install CodeDeploy agent
+    sudo apt-get -y install ruby
+    sudo apt-get -y install wget
+    cd /home/ubuntu
+    wget https://aws-codedeploy-eu-north-1.s3.amazonaws.com/latest/install
+    sudo chmod +x ./install 
+    sudo ./install auto
+else
+    echo "CodeDeploy agent already installed."
+fi
+
+echo "A" >> /home/ubuntu/status.txt
 
 # install golang
 sudo apt-get -y install golang-go
+echo "Installed Golang" >> /home/ubuntu/status.txt
 
 # install nodeJS and nvm
 sudo apt -y install nodejs
+echo "nodeJS installed successfully" >> /home/ubuntu/status.txt
 sudo apt -u install npm
+echo "npm installed successfully" >> /home/ubuntu/status.txt
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+echo "nvm installed successfully" >> /home/ubuntu/status.txt
 
 # install MongoDB
 sudo apt -y install mongodb
+echo "mongodb installed successfully" >> /home/ubuntu/status.txt
