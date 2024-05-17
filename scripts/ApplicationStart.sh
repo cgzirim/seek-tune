90eea4e991bc3470e7090bab73ca33e10a721bf6
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-05-17 13:12:10 +0100

    Update ApplicationStart.sh

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 start_backend() {
+    cd /home/ubuntu/song-recognition
     go build -tags netgo -ldflags '-s -w' -o app
     nohup ./app > backend.log 2>&1 &
     echo "Backend started successfully."
 }
 
 start_client() {
-    cd /client
+    cd /home/ubuntu/song-recognition/client
     nvm use 16
     npm run build
     nohup serve -s build > client.log 2>&1 &
