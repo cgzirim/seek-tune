a6a9bfbe3aa94b65f88a5e75674c35ad52f1fe6c
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-05-17 12:54:41 +0100

    Update BeforeInstall.sh

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -33,5 +33,14 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 echo "nvm installed successfully" >> /home/ubuntu/status.txt
 
 # install MongoDB
-sudo apt -y install mongodb
+sudo apt-get install gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongod 
+sudo apt-get install -y mongosh
+
 echo "mongodb installed successfully" >> /home/ubuntu/status.txt
