76944f547517cd3772533cf8b1d028af7c51fc03
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-05-18 07:14:55 +0100

    Update ApplicationStart.sh

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -9,16 +9,9 @@ start_backend() {
 
 start_client() {
     cd /home/ubuntu/song-recognition/client
-    touch client.txt
-
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    
-    nvm install 16
-    nvm use 16
     npm install
     npm run build
     nohup serve -s build > client.log 2>&1 &
 }
 
-start_backend && start_client
+start_backend
