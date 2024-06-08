3a439c9738a7b5b5b19e094008bf52156d707e83
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-06-08 14:01:37 +0100

    update .

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# HTTP_PID=$(sudo lsof -t -i:5000)
-# HTTPS_PID=$(sudo lsof -t -i:4443)
+echo "Hello server" > /home/ubuntu/started.txt
+
+HTTP_PID=$(sudo lsof -t -i:5000)
+HTTPS_PID=$(sudo lsof -t -i:4443)
 
 
-# if [ -n "$HTTP_PID" ]; then
-#   sudo kill -9 $HTTP_PID
-# fi
+if [ -n "$HTTP_PID" ]; then
+  sudo kill -9 $HTTP_PID
+fi
 
-# if [ -n "$HTTPS_PID" ]; then
-#   sudo kill -9 $HTTPS_PID
-# fi
-
-echo "Hello server"
+if [ -n "$HTTPS_PID" ]; then
+  sudo kill -9 $HTTPS_PID
+fi
