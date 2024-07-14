450caaff6ef6a79b9c066cf9cb683e14dbdebcf4
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-07-14 15:04:21 +0100

    Move RecordData struct to models to avoid circular import

--- models/models.go
+++ models/models.go
@@ -4,3 +4,11 @@ type Couple struct {
 	AnchorTimeMs uint32
 	SongID       uint32
 }
+
+type RecordData struct {
+	Audio      string  `json:"audio"`
+	Duration   float64 `json:"duration"`
+	Channels   int     `json:"channels"`
+	SampleRate int     `json:"sampleRate"`
+	SampleSize int     `json:"sampleSize"`
+}
