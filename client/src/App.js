5caa90f954de9f960abb914606609c9a03b2338a
Paul Irish <commits@paul.irish>
2024-08-04 11:24:25 -0700

    Use new name within the web client

--- client/src/App.js
+++ client/src/App.js
@@ -244,7 +244,7 @@ function App() {
   return (
     <div className="App">
       <div className="TopHeader">
-        <h1 style={{ color: "#374151" }}>!Shazam</h1>
+        <h1 style={{ color: "#374151" }}>SeekTune 🎵</h1>
         <h4 style={{ display: "flex", justifyContent: "flex-end" }}>
           <AnimatedNumber includeComma={true} animateToNumber={totalSongs} />
           &nbsp;Songs
